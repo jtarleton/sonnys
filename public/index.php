63eb4fc11a489d909a2d418fb610ea6440874d3a
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-15 16:54:48 -0400

    updates

--- public/index.php
+++ public/index.php
@@ -6,10 +6,25 @@ require __DIR__ . '/../vendor/autoload.php';
 use Sonnys\Application\Widget;
 use Sonnys\Application\Support\Widget as SupportWidget;
 
+
+
+use Sonnys\Application\Widget;
+use Sonnys\Application\WidgetFactory;
+use Sonnys\Application\Support\Widget as SupportWidget;
+use Sonnys\Appliction\Horse;
+use Sonnys\Application\Greeter;
+use Sonnys\Application\InformalGreeter;
+use Sonnys\Application\Pegasus;
+use Sonnys\Application\Pet;
+use Sonnys\Application\Person;
+use Sonnys\Application\Order;
+
 $widget = new Widget();
-/*
-$widget = WidgetFactory::Create(); 
+$supportWidget = new SupportWidget();
+
+$widget = WidgetFactory::Create();
 $supportWidget = WidgetFactory::Create('Support');
+
 
 $widget = new Widget();
 $supportWidget = new Widget();
@@ -21,8 +36,7 @@ $pet = new Pet();
 $person = new Person();
 
 
-echo "Greeting $greeter->getTarget()"
-echo $greeter->sayHello()
-echo "Greeting $informalGreeter->getTarget()"
-echo $informalGreeter->sayHello();
-*/
+echo "Greeting" . $greeter->getTarget() . '<br />';
+echo $greeter->sayHello() .'<br />';
+echo "Greeting " . $informalGreeter->getTarget() . '<br />';
+echo $informalGreeter->sayHello() . '<br />';
