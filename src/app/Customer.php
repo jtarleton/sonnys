d91eb6d6f4875a87cc00309aee77c07ea29749f0
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-16 17:59:56 -0400

    add class

---
+++ src/app/Customer.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Customer {
+
+	protected $_id, $_firstName, $_lastName, $_createdDate;
+
+	public function __construct($firstName, $lastName, $id) {
+		$this->_firstName = $firstName;
+		$this->_lastName = $lastName;
+		$this->_id = $id;
+		$this->_createdDate = new \DateTimeImmutable('now');
+
+	}
+
+	// GET and SET methods here...
+
+	public function getId() : int {
+		return $this->_id;
+	}
+}
