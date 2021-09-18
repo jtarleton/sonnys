e177947f4d782464771934b5947b09fdd46d2312
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-18 16:36:28 -0400

    updates, db class

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/bootstrap.php';
 require __DIR__ . '/templates/header.php';
 
 $classes = get_declared_classes();
-$action = new Action();
+
 $action->render();
 
 echo '<br />Done.'; 
