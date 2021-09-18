438491504b0a4856e597bf0308018ef0715a58ef
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-18 16:35:37 -0400

    updates, db class

--- src/app/Action.php
+++ src/app/Action.php
@@ -2,43 +2,44 @@
 
 namespace Sonnys\Application;
 
-use \Sonnys\Application\OrderLoader;
-use \Sonnys\Application\Customer;
-
-
+// Widgets app
 use Sonnys\Application\Widget;
 use Sonnys\Application\Support\SupportBase;
-
+use Sonnys\Application\WidgetFactory;
 use Sonnys\Application\Support\Widget as SupportWidget;
 
+// Greeter app
+use Sonnys\Application\InformalGreeter;
+use Sonnys\Application\GreeterInterface;
+use Sonnys\Application\GreeterTrait;
+
+// Exam app
 use Sonnys\Application\Exam;
 use Sonnys\Application\ExamBuilder;
 use Sonnys\Application\ExamBuilderInterface;
 
-use Sonnys\Application\WidgetFactory;
-
-use Sonnys\Application\EmployeeInterface;
-use Sonnys\Application\Employee;
-
+// Pegasus app
+use Sonnys\Application\Bird;
 use Sonnys\Application\Horse;
 use Sonnys\Application\Greeter;
-use Sonnys\Application\InformalGreeter;
-use Sonnys\Application\GreeterInterface;
-use Sonnys\Application\GreeterTrait;
+
 use Sonnys\Application\Pegasus;
 use Sonnys\Application\PegasusInterface;
 
-use Sonnys\Application\Pet;
+// nameable app
 use Sonnys\Application\Person;
-
+use Sonnys\Application\EmployeeInterface;
+use Sonnys\Application\Employee;
+use Sonnys\Application\Pet;
 use Sonnys\Application\Nameable;
 use Sonnys\Application\NameInfo;
 
-
-use Sonnys\Application\Customer;
+// order tracking app 
+use \Sonnys\Application\OrderLoader;
+use \Sonnys\Application\Customer;
 use Sonnys\Application\Order;
-use Sonnys\Application\OrderLoader;
 
+// universal remote control / tv app
 use Sonnys\Application\RemoteControl;
 use Sonnys\Application\RemoteControlUser;
 use Sonnys\Application\CableBox;
