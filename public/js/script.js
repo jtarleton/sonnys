e438d07616da52e1f453858f39da03fc6ef2f63b
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-18 17:43:19 -0400

    updates

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,15 @@
 var hash = jQuery(location).attr('hash');
 alert(hash);
 
+if (hash.length) {
+  //Toggle tab link
+  jQuery('a[href$=' + hash + ']').addClass('nav-tab-active').siblings().removeClass('nav-tab-active');
+
+  //Toggle target tab
+  jQuery(jQuery('a[href$=' + hash + ']').attr('href')).addClass('active').siblings().removeClass('active'); 
+}
+
+
 
 jQuery('.nav-tab').click(function(e) {
   //Toggle tab link
