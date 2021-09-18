d1ace3d4d4ed8e52d80ecd7c411b25ff3674c891
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-18 16:49:28 -0400

    updates, db class

--- public/index.php
+++ public/index.php
@@ -6,8 +6,35 @@ require __DIR__ . '/bootstrap.php';
 require __DIR__ . '/templates/header.php';
 
 $classes = get_declared_classes();
+?>
+<h2 class="nav-tab-wrapper">
+	<a href="#tab-1" class="nav-tab nav-tab-active">Greeter</a>
+	<a href="#tab-2" class="nav-tab">Pegasus</a>
+	<a href="#tab-3" class="nav-tab">Universal Remote</a>
+	<a href="#tab-4" class="nav-tab">Order Tracking</a>
+	<a href="#tab-5" class="nav-tab">Naming</a>
+	<a href="#tab-6" class="nav-tab">Widgets</a>
+</h2>
+<hr />
+<section id="tab-1" class="tab-content active">
+  <p><?php $action->render(); ?></p>
+</section>
+<section id="tab-2" class="tab-content">
+  <p>Pegasus.</p>
+</section>
+<section id="tab-3" class="tab-content">
+  <p>Universal Remote.</p>
+</section>
 
-$action->render();
-
+<section id="tab-4" class="tab-content">
+  <p>Order Tracking.</p>
+</section>
+<section id="tab-5" class="tab-content">
+  <p>Naming.</p>
+</section>
+<section id="tab-6" class="tab-content">
+  <p>Widgets.</p>
+</section>
+<?php
 echo '<br />Done.'; 
 require __DIR__ . '/templates/footer.php';
--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,13 @@
 
 
 
+jQuery('.nav-tab').click(function(e) {
+  //Toggle tab link
+  jQuery(this).addClass('nav-tab-active').siblings().removeClass('nav-tab-active');
 
+  //Toggle target tab
+  jQuery(jQuery(this).attr('href')).addClass('active').siblings().removeClass('active');
+});
 
 
 
