5b445006f661c4e4ca352373c8caeea190d6809e
James Tarleton <jtarleton.consultant@dot.nyc.gov>
2021-09-18 16:05:05 -0400

    updates, db class

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 // Autoload files using the Composer autoloader.
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . 'bootstrap.php';
-require __DIR__ . './templates/header.php';
+require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/templates/header.php';
 
 $classes = get_declared_classes();
 $action = new Action();
